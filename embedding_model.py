6cc0c8564c3df41ffcbcf61d10c9fc5051c92581
lzx <a0908287570@gmaill.com>
2025-11-24 04:28:04 +0800

    dwad

--- embedding_model.py
+++ embedding_model.py
@@ -1,21 +1,15 @@
-import numpy as np
-import onnxruntime as ort
 from huggingface_hub import hf_hub_download
+import onnxruntime as ort
+import numpy as np
 
-# 使用 bge-micro 單文件模型（不會碎片）
-repo_id = "BAAI/bge-micro"
+repo_id = "BAAI/bge-m3"
 model_file = "onnx/model.onnx"
 
-# 下載 ONNX 模型
 model_path = hf_hub_download(repo_id=repo_id, filename=model_file)
 
-# 建立 ONNX Runtime Session
 session = ort.InferenceSession(model_path, providers=["CPUExecutionProvider"])
 
 def embed_text(text: str):
-    # 模型要求輸入為字串陣列
     inputs = {"input_text": np.array([text])}
     outputs = session.run(None, inputs)
-
-    # 第一個 output 是 embedding，[0]取 batch 裡第一筆
     return outputs[0][0].tolist()
